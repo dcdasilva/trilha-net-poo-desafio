b6d5ce3fa39e064e63406e7f41a4b3bc32677e15
Davi C. Silva <davic_silva@hotmail.com>
2025-10-17 17:39:51 -0300

    Resolução do Desafio DIO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+var nokia = new Nokia("+55 16 123123123", "A-20", "1254544", 512);
+nokia.InstalarAplicativo("WhatsApp");
+nokia.InstalarAplicativo("Instagram");
+nokia.InstalarAplicativo("LinkedIn");
+nokia.Ligar();
+nokia.ReceberLigacao();
+
+
+var iPhone = new Iphone("+55 21 2121321321", "I90", "1358755", 1024);
+iPhone.InstalarAplicativo("WhatsApp");
+iPhone.InstalarAplicativo("Instagram");
+iPhone.InstalarAplicativo("LinkedIn");
+iPhone.Ligar();
+iPhone.ReceberLigacao();
